23ade916a4ad1a70d0578b4932fd61f07a963971
Loyal codes <loyalcodestudio@gmail.com>
2022-03-01 11:09:54 +0200

    Added settings action in store

--- src/store/actions/settingsActions.js
+++ src/store/actions/settingsActions.js
@@ -1,11 +1,19 @@
 import { 
-  MODAL_UPDATE
+  MODAL_UPDATE,
+  UPDATE_CURRENT_APP
 } from './types';
 
 
 export const modalUpdate = (payload) => dispatch => {
   dispatch({
     type: MODAL_UPDATE,
+    payload
+  })
+};
+
+export const updateDateCurrentApp = (payload) => dispatch => {
+  dispatch({
+    type: UPDATE_CURRENT_APP,
     payload
   })
 };

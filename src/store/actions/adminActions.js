87bd7fdd7b89a920d0363a66051ab8cdd2647900
Loyal codes <loyalcodestudio@gmail.com>
2022-03-02 11:59:32 +0200

    Added Fetch uses to store

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,4 +1,6 @@
-import { FETCH_ORGANIZATIONS } from '../actions/types';
+import { FETCH_ORGANIZATIONS,
+         FETCH_USERS
+       } from '../actions/types';
 import { fireStore } from "../../firebase";
 
 export const fetchOrganizations =  () => dispatch => {
@@ -8,6 +10,19 @@ export const fetchOrganizations =  () => dispatch => {
       if ( !(snapshot.empty && snapshot.metadata.fromCache) ) {
         dispatch({
           type: FETCH_ORGANIZATIONS,
+          payload: snapshot.docs.map(doc => { return {...doc.data(), id: doc.id} })
+        })
+      }
+    })
+    .catch( err => console.log(err) )
+  }
+  export const fetchUsers =  () => dispatch => {
+    fireStore.collection('/users')
+    .get()
+    .then(snapshot => {
+      if ( !(snapshot.empty && snapshot.metadata.fromCache) ) {
+        dispatch({
+          type: FETCH_USERS,
           payload: snapshot.docs.map(doc => { return {...doc.data(), id: doc.id} })
         })
       }

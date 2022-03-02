4f977cf647985440748231c648502cf176302967
Loyal codes <loyalcodestudio@gmail.com>
2022-03-02 12:03:32 +0200

    Created organization page

---
+++ src/pages/Organization.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// components
+
+class Organization extends React.Component {
+    render(){
+
+        return (
+            <div>Organization content will be here</div>
+        )   
+    }
+}
+
+export default Organization
+
--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -16,6 +16,7 @@ export const fetchOrganizations =  () => dispatch => {
     })
     .catch( err => console.log(err) )
   }
+  
   export const fetchUsers =  () => dispatch => {
     fireStore.collection('/users')
     .get()
